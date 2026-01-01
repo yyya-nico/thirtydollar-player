87bf1a57c1d53b350f6d9dcd40fa233930f9e78c
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2026-01-02 01:45:00 +0900

    HTMLの整理、iframe要素ではなくaudio要素を使うように

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ interface Sound {
     source: string;
     img?: string;
     emoji?: string;
-    tags?: 'note' | 'percussion'[];
+    tags?: ('note' | 'percussion')[];
 }
 
 const jsonTextArea = document.getElementById('json') as HTMLTextAreaElement;
-const filePlayer = document.getElementById('file-player') as HTMLIFrameElement;
+const player = document.getElementById('player') as HTMLIFrameElement;
 const soundsList = document.getElementById('sounds-list') as HTMLUListElement;
 
 const websiteBase = 'https://thirtydollar.website';
@@ -23,8 +23,10 @@ jsonTextArea.addEventListener('change', () => {
     try {
         const json = JSON.parse(jsonTextArea.value) as Sound[];
         soundsList.innerHTML = json.map(sound => `<li>
-    <span>${sound.name}</span><br>
-    <button value="${sound.id}">${sound.emoji || `<img src="${websiteBase}/icons/${sound.img || sound.id}.png" alt="${sound.name}" width="48" height="48">`}</button>
+    <button value="${sound.id}" title="${sound.name}">
+        <span class="name">${sound.name}</span><br>
+        ${sound.emoji ? `<span class="emoji">${sound.emoji}</span>` : `<img src="${websiteBase}/icons/${sound.img || sound.id}.png" alt="${sound.name}" width="48" height="48">`}
+    </button>
 </li>`).join('\n');
     } catch (error) {
         soundsList.textContent = 'Invalid JSON';
@@ -35,6 +37,6 @@ soundsList.addEventListener('click', e => {
     const target = e.target as HTMLElement;
     const button = target.closest('button');
     if (button) {
-        filePlayer.src = `${websiteBase}/sounds/${button.value}.wav`;
+        player.src = `${websiteBase}/sounds/${button.value}.wav`;
     }
 });
